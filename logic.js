7549afc5d629837e887166f5f8b032d282831d4c
Sudoku Shogun <ancientasm@yahoo.com>
2014-10-06 07:35:21 -0400

    now playable and works on mobile

--- logic.js
+++ logic.js
@@ -28,6 +28,23 @@ function swap(container, x, y) {
     container[y] = temp;
 }
 
+function lookup(matrix, pos) {
+    var r = Math.floor(pos / matrix.length),
+        c = pos - r * matrix.length;
+    return matrix[r][c];
+}
+
+function testme(event) {
+    var funfun = event,
+        cellId = funfun.srcElement.parentElement.id.substr(5),
+        keypress = Number(String.fromCharCode(event.keyCode));
+
+    console.log(keypress, cellId, lookup(solution, cellId));
+    if (lookup(solution, cellId) !== keypress) {
+        event.preventDefault();
+    }
+}
+
 function permuteBoard(partialBoard, fullBoard) {
     //things to permute:
     //permute alphabet
@@ -37,11 +54,14 @@ function permuteBoard(partialBoard, fullBoard) {
     //permute rows within block
     //permute columns within block
 
-    //permute alphabet
+    //permute alphabet - not the best permutation, but this isn't exactly military-grade Sudoku
     var alpha = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], //i maps to alpha[i], so 0 maps to 0 to preserve empty squares
         seq = String(Math.ceil(Math.random()*1e9)).split(''), //9-digit random number sequence
         temp;
 
+    while (seq.length < 9) {
+        seq.unshift(0); //leading zeroes
+    }
     seq = seq.map(function(i){ return (i % 9)+1;}); //narrow range to numbers between 1 - 9
 
     for(var i = 0; 9 > i; ++i) {

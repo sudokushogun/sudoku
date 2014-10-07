7d498c635ba2dfd6ff6806105bef1c34d71d6bf7
Sudoku Shogun <ancientasm@yahoo.com>
2014-10-07 18:58:06 -0400

    permutations on sudoku board

--- logic.js
+++ logic.js
@@ -22,19 +22,15 @@ var shown = [
         [3, 4, 5, 2, 8, 6, 1, 7, 9]
     ];
 
-function swap(container, x, y) {
-    var temp = container[x];
-    container[x] = container[y];
-    container[y] = temp;
-}
-
 function lookup(matrix, pos) {
+    //lookup 2D matrix by offset from 0
     var r = Math.floor(pos / matrix.length),
         c = pos - r * matrix.length;
     return matrix[r][c];
 }
 
-function testme(event) {
+function checkSolution(event) {
+    //check typed user entry against position in solution matrix
     var funfun = event,
         cellId = funfun.srcElement.parentElement.id.substr(5),
         keypress = Number(String.fromCharCode(event.keyCode));
@@ -45,29 +41,77 @@ function testme(event) {
     }
 }
 
-function permuteBoard(partialBoard, fullBoard) {
-    //things to permute:
-    //permute alphabet
-    //block: a 3x3 grid of numbers - sudoku is a 3x3 grid of Latin square blocks
-    //permute blocks by row
-    //permute blocks by columns
-    //permute rows within block
-    //permute columns within block
 
-    //permute alphabet - not the best permutation, but this isn't exactly military-grade Sudoku
-    var alpha = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], //i maps to alpha[i], so 0 maps to 0 to preserve empty squares
-        seq = String(Math.ceil(Math.random()*1e9)).split(''), //9-digit random number sequence
-        temp;
-
-    while (seq.length < 9) {
-        seq.unshift(0); //leading zeroes
-    }
-    seq = seq.map(function(i){ return (i % 9)+1;}); //narrow range to numbers between 1 - 9
-
-    for(var i = 0; 9 > i; ++i) {
-        swap(alpha, i+1, seq[i]);   //shuffle alphabet array based on sequence
-    }
-    return alpha;
+function swapElements(firstContainer, firstIndex, secondContainer, secondIndex) {
+    var temp = firstContainer[firstIndex];
+    firstContainer[firstIndex] = secondContainer[secondIndex];
+    secondContainer[secondIndex] = temp;
 }
 
-console.log(permuteBoard(shown, solution));
+function swapVectors(partialBoard, fullBoard, firstIndex, secondIndex, isColumn) {
+    //swap two vectors on the board
+    if (!isColumn) {
+        swapElements(partialBoard, firstIndex, partialBoard, secondIndex);    //swap arrays
+        swapElements(fullBoard, firstIndex, fullBoard, secondIndex);
+    } else {
+        for(var i = 0; partialBoard.length > i; ++i) {
+            //partialBoard and fullBoard should have the same dimensions
+            swapElements(partialBoard[i], firstIndex, partialBoard[i], secondIndex);
+            swapElements(fullBoard[i], firstIndex, fullBoard[i], secondIndex);
+        }
+    }
+}
+
+function permuteBoard(partialBoard, fullBoard) {
+    permuteNumbers(partialBoard, fullBoard);
+    permuteVectors(partialBoard, fullBoard);
+}
+
+function permuteVectors(partialBoard, fullBoard) {
+    var rowShuffle, colShuffle, blockShuffle;
+
+    for(var start = 0; 9 > start; start += 3) {
+        //get a random number from 0 - 5 for three-element permutation: 3! = 6
+        rowShuffle = Math.floor(Math.random() * 10) % 6;
+        colShuffle = Math.floor(Math.random() * 10) % 6;
+        blockShuffle = Math.floor(Math.random() * 10) % 6;
+
+        //permutes rows and columns within blocks
+        swapVectors(partialBoard, fullBoard, start, start + (rowShuffle % 3), false);    //shuffle first row
+        swapVectors(partialBoard, fullBoard, start + 1, start + 1 + (rowShuffle % 2), false);   //shuffle second row
+        swapVectors(partialBoard, fullBoard, start, start + (colShuffle % 3), true);    //shuffle first column
+        swapVectors(partialBoard, fullBoard, start + 1, start + 1 + (colShuffle % 2), true);   //shuffle second column
+
+        //permute blocks
+        for(var i = 0; 3 > i; ++i) {
+            swapVectors(partialBoard, fullBoard, i, (blockShuffle % 3) * 3 + i, false); //permute first block by row
+            swapVectors(partialBoard, fullBoard, i, (1 + (blockShuffle % 2)) * 3 + i, false);   //permute second block by row
+            swapVectors(partialBoard, fullBoard, i, (blockShuffle % 3) * 3 + i, true);  //permute first block by column
+            swapVectors(partialBoard, fullBoard, i, (1 + (blockShuffle % 2)) * 3 + i, true);    //permute second block by column
+        }
+    }
+}
+
+function permuteNumbers(partialBoard, fullBoard) {
+    //permutes number sequence on boards
+    var alpha = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], //alphabet array of numbers for permutation
+        seq = String(Math.floor(Math.random()*1e9)).split(''); //9-digit random number sequence (excluding leading zeroes)
+
+    while (seq.length < 9) {
+        seq.unshift(0); //handle leading zeroes
+    }
+    seq = seq.map(function(i){ return (i % 9) + 1;}); //narrow range to numbers between 1 - 9
+
+    for(var i = 0; 9 > i; ++i) {
+        swapElements(alpha, i+1, alpha, seq[i]);   //shuffle alphabet array based on sequence ignore 0's (empty squares)
+    }
+    for(var i = 0; 9 > i; ++i) {
+        for(var j = 0; 9> j; ++j) {
+            partialBoard[i][j] = alpha[partialBoard[i][j]]; //shuffle alphabet
+            fullBoard[i][j] = alpha[fullBoard[i][j]];
+        }
+    }
+}
+
+//console.log(permuteBoard(shown, solution));
+permuteBoard(shown, solution);
